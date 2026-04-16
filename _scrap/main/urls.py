bdcb612e72643d12149d47386870e12604f34f38
yashthinktanker <yashparmar.thinktanker@gmail.com>
2026-04-16 18:31:42 +0530

    new future add

--- _scrap/main/urls.py
+++ _scrap/main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('download/', views.download_csv, name='download_csv'),
     path('single-event/', views.single_event_data, name='single_event_data'),
+    path('compare-files/', views.compare_files, name='compare_files'),
     path('download/progress/<str:job_id>/', views.download_progress, name='download_progress'),
 ]
